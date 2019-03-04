62469bf5dde35ffc3cbcf0575dbc46bbefcaef3b
ASZANIAW <arturszaniawski85@gmail.com>
2019-03-04 08:26:33 +0100

    changes in portfolio App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,47 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+
 
 class App extends Component {
+
+
+  constructor() {
+    super();
+    this.state = {displayBio: false};
+    this.readMore = this.readMore.bind(this);
+    this.showLess = this.showLess.bind(this);
+
+  }
+
+  readMore() {
+    this.setState({displayBio: true})
+  }
+
+  showLess() {
+    this.setState({displayBio: false})
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h1> Hello!</h1>
+        <p>My name is Artur and I'm Software Engineer!</p>
+        <p>I'm always looking forward to working on meaningful projects!</p>
+        {
+          this.state.displayBio ? (
+            <div>
+              <p>I live in Poznan and I code every day!</p>
+              <p>My favourite language is Java but i want to develop in Frontend too!</p>
+              <p>Besides coding I like sport</p>
+              <button onClick={this.showLess}>Show less</button>
+            </div>
+          ) : (
+            <div>
+              <button onClick={this.readMore}>Read more</button>
+            </div>
+          )
+        }
       </div>
-    );
+    )
   }
 }
 
