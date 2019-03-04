ea92c38cfd2e247992bc37ae1b66064d965f8894
ASZANIAW <arturszaniawski85@gmail.com>
2019-03-04 09:08:22 +0100

    first component refactored

--- src/App.js
+++ src/App.js
@@ -3,24 +3,14 @@ import React, {Component} from 'react';
 
 class App extends Component {
 
+  state = {displayBio: false};
 
-  constructor() {
-    super();
-    this.state = {displayBio: false};
-    this.readMore = this.readMore.bind(this);
-    this.showLess = this.showLess.bind(this);
-
+  toggleDisplayBio = () => {
+    this.setState({displayBio: !this.state.displayBio})
   }
 
-  readMore() {
-    this.setState({displayBio: true})
-  }
 
-  showLess() {
-    this.setState({displayBio: false})
-  }
-
-  render() {
+  render(){
     return (
       <div>
         <h1> Hello!</h1>
@@ -32,11 +22,11 @@ class App extends Component {
               <p>I live in Poznan and I code every day!</p>
               <p>My favourite language is Java but i want to develop in Frontend too!</p>
               <p>Besides coding I like sport</p>
-              <button onClick={this.showLess}>Show less</button>
+              <button onClick={this.toggleDisplayBio}>Show less</button>
             </div>
           ) : (
             <div>
-              <button onClick={this.readMore}>Read more</button>
+              <button onClick={this.toggleDisplayBio}>Read more</button>
             </div>
           )
         }
